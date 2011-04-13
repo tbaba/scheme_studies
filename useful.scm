fae939b4a5da0f0996141b4b0063132d408383a5
Tatsuro BABA <t.baba@grooves.co.jp>
2011-04-13 17:39:15 +0900

    1st day done up to p.77

--- useful.scm
+++ useful.scm
@@ -1,5 +1,58 @@
-(define atom?
-  (lambda (x)
-	(and (not (pair? x)) (not (null? x)))))
+(define atom? (lambda (x) (and (not (pair? x)) (not (null? x)))))
 
+(define add1 (lambda (x) (+ x 1)))
 
+(define sub1 (lambda (x) (- x 1)))
+
+(define plus
+  (lambda (x y)
+	(cond
+	  ((zero? y) x)
+	  (else
+		(add1 (plus x (sub1 y)))))))
+
+(define minus
+  (lambda (x y)
+	(cond
+	  ((zero? y) x)
+	  (else
+		(sub1 (minus x (sub1 y)))))))
+
+(define addtup
+  (lambda(tup)
+	(cond
+	  ((null? tup) 0)
+	  (else
+		(plus (car tup) (addtup (cdr tup)))))))
+
+(define multiple
+  (lambda(x y)
+	(cond
+	  ((zero? y) 0)
+	  (else
+		(plus x (multiple x (sub1 y)))))))
+
+(define tup+
+  (lambda(tup1 tup2)
+	(cond
+	  ((and (null? tup1) (null? tup2)) '())
+	  (else
+		(cons
+		  (plus (car tup1) (car tup2))
+		  (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define tup++
+  (lambda(tup1 tup2)
+	(cond
+	  ((null? tup1) tup2)
+	  ((null? tup2) tup1)
+	  (else
+		(cons
+		  (plus (car tup1) (car tup2))
+		  (tup+ (cdr tup1) (cdr tup2)))))))
+(define power
+  (lambda(x y)
+	(cond
+	  ((zero? y) 1)
+	  (else
+		(multiple x (power x (sub1 y)))))))
